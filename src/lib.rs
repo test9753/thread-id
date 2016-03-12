6ce08bb3714e873e23a68f3ff72143e9aabf9c8c
Ruud van Asseldonk <dev@veniogames.com>
2016-03-12 17:26:53 +0100

    Initial commit with basic Unix version
    This allows retrieving the thread ID on platforms with pthread support.

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+// Thread-ID -- Get a unique thread ID
+// Copyright 2016 Ruud van Asseldonk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License has been included in the root of the repository.
+
+//! Thread-ID: get a unique ID for the current thread.
+//!
+//! # Example
+//!
+//! ```
+//! use std::sync::mpsc;
+//! use std::thread;
+//! use thread_id;
+//!
+//! let (tx, rx) = mpsc::channel();
+//! thread::spawn(move || tx.send(thread_id::get()).unwrap()).join().unwrap();
+//!
+//! let main_tid = thread_id::get();
+//! let other_tid = rx.recv().unwrap();
+//! assert!(main_tid != other_tid);
+//! println!("main thread has id {}, other had id {}", main_tid, other_tid);
+//! ```
+
+#![warn(missing_docs)]
+
+extern crate libc;
+
+/// Returns a number that is unique per thread.
+pub fn get() -> u64 {
+    get_internal()
+}
+
+#[cfg(unix)]
+fn get_internal() -> u64 {
+    unsafe { libc::pthread_self() as u64 }
+}
