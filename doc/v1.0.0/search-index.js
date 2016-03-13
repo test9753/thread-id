3853a987aaf52475359c893b3ee573fce506282a
Ruud van Asseldonk <dev@veniogames.com>
2016-03-13 22:26:00 +0100

    Add Thread-ID v1.0.0 API reference
    The last part of the path should actually have an underscore instead of
    a hyphen, but I set the documentation URLs already, so I changed the
    directory name. This should be fixed if there ever is a new release.

---
+++ doc/v1.0.0/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex['thread_id'] = {"items":[[5,"get","thread_id","Returns a number that is unique to the calling thread.",null,{"inputs":[],"output":{"name":"u64"}}]],"paths":[]};
+initSearch(searchIndex);
