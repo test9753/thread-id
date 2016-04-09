c8138a0568fa6cc1c22e2efb9dcf1818cd5f006c
Ruud van Asseldonk <dev@veniogames.com>
2016-04-09 11:55:28 +0200

    Add Thread-ID v2.0.0 API reference

---
+++ doc/v2.0.0/search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["thread_id"] = {"doc":"Thread-ID: get a unique ID for the current thread.","items":[[5,"get","thread_id","Returns a number that is unique to the calling thread.",null,{"inputs":[],"output":{"name":"usize"}}]],"paths":[]};
+initSearch(searchIndex);
